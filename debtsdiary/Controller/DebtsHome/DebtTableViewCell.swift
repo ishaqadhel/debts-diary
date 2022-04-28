144ab0c79558ee44ec6ef04473b6569ae3fa8090
ishaqadhel <49280352+ishaqadhel@users.noreply.github.com>
2022-04-28 23:49:53 +0700

    feat: add debts home screen

---
+++ debtsdiary/Controller/DebtsHome/DebtTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DebtTableViewCell.swift
+//  debtsdiary
+//
+//  Created by Ishaq Adheltyo on 28/04/22.
+//
+
+import UIKit
+
+class DebtTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
