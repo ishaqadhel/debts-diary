478e060ee7ea02795b862c599a88bf2c14a13d82
ishaqadhel <49280352+ishaqadhel@users.noreply.github.com>
2022-04-28 15:04:27 +0700

    feat: add splash screen
    * feat: init swift storyboard project
    
    * feat: add launchscreen

---
+++ debtsdiary/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  debtsdiary
+//
+//  Created by Ishaq Adheltyo on 28/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
