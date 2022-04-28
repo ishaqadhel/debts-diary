0de06a644609a012ec1b7206ca57ce3875000235
ishaqadhel <ishaq.adhel@gmail.com>
2022-04-28 19:35:30 +0700

    feat: add first setup screen

---
+++ debtsdiary/Controller/FirstSetupViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FirstSetupViewController.swift
+//  debtsdiary
+//
+//  Created by Ishaq Adheltyo on 28/04/22.
+//
+
+import UIKit
+
+class FirstSetupViewController: UIViewController {
+
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var currencyTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
